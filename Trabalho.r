af6bc71a7137454e2e5aa24d7ea6f09152eacf55
Marcos Moraes <124010289+MoraesMarcos@users.noreply.github.com>
2024-05-29 11:57:42 -0300

    [+] Add Trabalho.r

---
+++ Trabalho.r
@@ -0,0 +1,35 @@
+## 1. Construam um gráfico temporal da série cronológica e descrevam as suas
+# principais características. 
+
+install.packages("forecast")
+library(forecast)
+
+data <- gas
+show(data)
+
+# Verificando estrutura da série temporal
+str(data)
+
+# Plotando gráfico temporal
+plot(data, main="Consumo mensal de gás na Austrália", ylab="Gás (milhões de metros cúbicos)", xlab="Ano")
+monthplot(gas, main="Variação sazonal mensal do consumo de gás", ylab="Gás (milhões de metros cúbicos)", xlab="Mês")
+
+
+## Tendência: A série apresenta uma tendência ascendente ao longo do tempo, indicando um aumento no consumo de gás na Austrália durante o período analisado.
+## Sazonalidade: Há uma forte componente sazonal, com padrões que se repetem anualmente, aumentando a cada ano, indicando a presença de uma sazonalidade multiplicativa.
+## Variabilidade: A amplitude das variações sazonais parece aumentar ao longo do tempo, sugerindo que a variabilidade no consumo de gás se torna mais pronunciada nos anos mais recentes.
+
+################################################################################
+
+## 2. Decomponham a série nas diferentes componentes, ilustrando graficamente a
+#decomposição efetuada. Comentem os resultados. 
+
+# Decompor a série temporal usando decompose
+data_decomp <- decompose(data, type="multiplicative")
+
+# Plotar a decomposição com decompose
+plot(data_decomp,"Decomposição da Série Temporal - Método decompose")
+
+# A tendência crescente mostra o aumento geral no consumo de gás ao longo dos anos.
+# A componente sazonal mostra um padrão que se repete anualmente.
+# A componente residual captura as variações que não são explicadas pela tendência ou sazonalidade, incluindo ruídos. Podemos ver inicialmente um decrescimento do resíduos até pouco antes dos anos 80, quando ele volta a crescer e variar cada vez mais a cada ano.
